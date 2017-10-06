7dbbfb755f3c0cbf77f634ddcf49c1a623e72322
wadson espindola <wadson.espindola@gmail.com>
2017-10-06 20:17:39 +0000

    rolling back commit to cf719bf

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,9 @@ class PagesController < ApplicationController
   def home
     @players ||= Player.order(params[:sort]).paginate(:page => params[:page], :per_page => 25)
     @batting_average ||= nil
+      respond_to do |format|
+        format.html
+        format.js
+        end
   end
 end
