834fcc37fd768978cdb47bb244072685dd92046e
wadson espindola <wadson.espindola@gmail.com>
2017-10-06 19:37:49 +0000

    remove default scope, set order DESC in pages#home

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
   def home
-    @players ||= Player.order(params[:sort]).paginate(:page => params[:page], :per_page => 25)
+    @players ||= Player.order(params[:sort], "avg DESC").paginate(:page => params[:page], :per_page => 25)
     @batting_average ||= nil
   end
 end
