69e3614bd64be3f6deb13ca190fc0d1fa64ca9fe
wadson espindola <wadson.espindola@gmail.com>
2017-10-06 19:00:57 +0000

    add default values to handle missing data when seeding

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,9 +1,28 @@
+# == Schema Information
+#
+# Table name: players
+#
+#  id         :integer          not null, primary key
+#  first      :string
+#  last       :string
+#  position   :string
+#  avg        :float
+#  hr         :integer
+#  rbi        :integer
+#  runs       :integer
+#  sb         :integer
+#  ops        :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  hits       :integer
+#  bats       :integer
+#
+
 class Player < ActiveRecord::Base
   before_create :calc_batting_avg
-
+  after_initialize :set_default_values, unless: :valid?
+  validates :first, :last, :position, :avg, :hr, :rbi, :runs, :sb, :ops, :hits, :bats, presence: true
   
-  
-
   
 private
 # Calculates Player's batting average
@@ -19,5 +38,16 @@ private
 # slugging_percentage = total_bases % at_bats
 
   def calc_batting_ops
+  end
+  
+  def set_default_values
+    self.avg ||= 0
+    self.hr ||= 0
+    self.rbi ||= 0
+    self.runs ||= 0
+    self.sb ||= 0
+    self.ops ||= 0
+    self.hits ||= 0
+    self.bats ||= 0
   end
 end
