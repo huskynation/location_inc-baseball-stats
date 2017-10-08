9f02182c7d9930ffc746633c5cee6a8e50f236e8
wadson espindola <wadson.espindola@gmail.com>
2017-10-08 14:27:08 +0000

    session works, registration still in progress...

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,9 @@
 //= require turbolinks
 //= require twitter/bootstrap
 //= require_tree .
+
+$(document).ready(function() {
+ $(‘#modal-login’).click(function() {
+   $(‘#modal’).modal(‘hide’);
+ });
+});
