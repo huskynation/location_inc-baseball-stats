5febb1bf13af9a826811e719c3f662e582f832b8
wadson espindola <wadson.espindola@gmail.com>
2017-10-09 05:15:47 +0000

    add ability to sort and flip direction

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,21 @@
 class PagesController < ApplicationController
   def home
-    @players ||= Player.order(params[:sort], "avg DESC").paginate(:page => params[:page], :per_page => 25)
+    @players ||= Player.order(sort_column + " " + sort_direction, "avg DESC").paginate(:page => params[:page], :per_page => 25)
     @batting_average ||= nil
       respond_to do |format|
         format.html
         format.js
-        end
+      end
   end
+  
+private
+
+def sort_column
+  params[:sort] || "name"
+end
+
+def sort_direction
+  params[:direction] || "asc"
+end
+
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,10 @@ module ApplicationHelper
     def paginate(collection, params= {})
         will_paginate collection, params.merge(:renderer => RemoteLinkPaginationHelper::LinkRenderer)
     end
+    
+    def sort_column(column, title=nil)
+      title ||= column.upcase
+      direction = column == params[:sort] && params[:direction] == 'ASC' ? "DESC" : "ASC"
+      link_to title, :sort => column, :direction => direction
+    end
 end
